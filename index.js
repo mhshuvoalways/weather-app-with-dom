8eea283621bcad67a6240d3b0b2e3f7ce1e53a9e
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-04-11 00:44:05 +0600

    static

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const app = express()
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('/history', require('./histotyRoute'))
+app.use("/history", require("./histotyRoute"));
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-)
-const db = mongoose.connection
-db.on('error', (err) => {
-    console.log(err)
-})
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => {
+  console.log(err);
+});
 db.once("open", () => {
-    console.log("Database connect Estabished")
-})
+  console.log("Database connect Estabished");
+});
 
-const PORT = process.env.PORT || 5000
+app.use(express.static(path.resolve(__dirname, "public")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`)
-})
+  console.log(`Running on port ${PORT}`);
+});
